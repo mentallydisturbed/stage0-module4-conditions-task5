485f17f06835c9a4f363da6a9a41d45c3bd82f8d
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-17 00:02:19 +0600

    fix2mod4task5

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -3,25 +3,23 @@ package school.mjc.stage0.conditions.task5;
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
         switch (month) {
-            case 1:
             case 2:
-            case 12:
-                System.out.println("Winter");
+                System.out.println(28);
                 break;
+            case 1:
             case 3:
-            case 4:
             case 5:
-                System.out.println("Spring");
-                break;
-            case 6:
             case 7:
             case 8:
-                System.out.println("Summer");
-                break;
-            case 9:
             case 10:
+            case 12:
+                System.out.println(31);
+                break;
+            case 4:
+            case 6:
+            case 9:
             case 11:
-                System.out.println("Fall");
+                System.out.println(30);
                 break;
             default:
                 System.out.println("wrong number!");
